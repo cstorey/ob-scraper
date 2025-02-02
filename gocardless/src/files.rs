d526a204662dcb720569ad70f5802c19a97b3bfb
Ceri Storey <git@noncrab.net>
2025-02-02 12:38:01 +0000

    Create with umask 0666 by default.

--- gocardless/src/files.rs
+++ gocardless/src/files.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, path::Path};
+use std::{fs::Permissions, io::Write, os::unix::fs::PermissionsExt, path::Path};
 
 use color_eyre::Result;
 use serde::Serialize;
@@ -15,7 +15,10 @@ pub(crate) async fn write_atomically<T: Serialize + Send + 'static>(
     spawn_blocking(move || -> Result<()> {
         let _entered = span.enter();
         let parent = path.parent().unwrap_or(".".as_ref());
-        let mut f = tempfile::NamedTempFile::new_in(parent)?;
+
+        let mut f = tempfile::Builder::new()
+            .permissions(Permissions::from_mode(0o666))
+            .tempfile_in(parent)?;
         serde_json::to_writer_pretty(&mut f, &state)?;
         f.flush()?;
         f.persist(&path)?;
